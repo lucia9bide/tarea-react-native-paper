19323466bb334f3f614fb7be3b7f3231f56d0703
Lucia Bide <luciabide9@gmail.com>
2026-06-12 10:12:38 -0300

    Creando los componentes e instalando la libreria

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { BookList } from "./screens/BookList/BookList";
+import { MyBooks } from "./screens/Mybooks/MyBooks";
+import { Profile } from "./screens/Profile/Profile";
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
